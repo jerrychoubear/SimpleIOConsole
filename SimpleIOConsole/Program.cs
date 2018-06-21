ea7333e0409386f6dc3ac504ea35ad0d822680c2
JJ <jerrychoubear@gmail.com>
2018-06-21 17:43:06 +0800

    File.ReadAllBytes

--- SimpleIOConsole/Program.cs
+++ SimpleIOConsole/Program.cs
@@ -8,9 +8,36 @@ namespace SimpleIOConsole
     {
         static void Main(string[] args)
         {
+            //NewMethod();
+
             try
             {
-                var folderPath = @"E:\temp\SimpleIOConsole\Output";
+                var imageByteArray = File.ReadAllBytes(
+                    @"E:\temp\854\PhysicalFolder_UploadFiles\009\009-20180620171122\未命名.png");
+
+                const string filename = @"E:\abc.png";
+                using (var stream = new FileStream(
+                                        filename, FileMode.CreateNew,
+                                        FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    Console.WriteLine($"寫入檔案{filename}開始...");
+                    stream.Write(imageByteArray, 0, imageByteArray.Length);
+                    Console.WriteLine($"寫入檔案{filename}完成");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static void NewMethod()
+        {
+            try
+            {
+                //var folderPath = @"E:\temp\SimpleIOConsole\Output";
+                var folderPath = @"\\127.0.0.1\854\Test";
                 var fileName = $"{DateTime.Now.ToString("yyyyMMddHHmmss")}.txt";
                 var outputDirectoryInfo = new DirectoryInfo(folderPath);
                 var outputFileInfo = new FileInfo(Path.Combine(folderPath, fileName));
