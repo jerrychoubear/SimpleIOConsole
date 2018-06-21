eb412cec61fd672ec647ba09d7a3f046ae1b65f2
JJ <jerrychoubear@gmail.com>
2018-06-21 14:28:04 +0800

    簡單字串寫入文字檔。

--- SimpleIOConsole/Program.cs
+++ SimpleIOConsole/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleIOConsole
 {
@@ -10,6 +8,38 @@ namespace SimpleIOConsole
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var folderPath = @"E:\temp\SimpleIOConsole\Output";
+                var fileName = $"{DateTime.Now.ToString("yyyyMMddHHmmss")}.txt";
+                var outputDirectoryInfo = new DirectoryInfo(folderPath);
+                var outputFileInfo = new FileInfo(Path.Combine(folderPath, fileName));
+
+                if (outputDirectoryInfo.Exists == false)
+                {
+                    outputDirectoryInfo.Create();
+                }
+
+                var datumString = @"Hello World!!";
+                var datum = Encoding.Default.GetBytes(datumString);
+
+                using (var stream = new FileStream(
+                                        outputFileInfo.FullName, FileMode.CreateNew,
+                                        FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    Console.WriteLine($"寫入檔案{outputFileInfo.FullName}開始...");
+                    stream.Write(datum, 0, datum.Length);
+                    Console.WriteLine($"寫入檔案{outputFileInfo.FullName}完成");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
